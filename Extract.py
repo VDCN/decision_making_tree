abbcc050fd7220ea4e40ca8f0d161093e395e688
wuxuan <603491092@qq.com>
2014-07-14 12:07:32 +0800

    1.修复了再删除sf时的bug，成功剔除了顺丰邮箱

--- Extract.py
+++ Extract.py
@@ -6,11 +6,19 @@ import database
 
 db = database.Connection(host='192.168.30.239',database='flyphp',user='dev_query',password='OWl32ud0zW8uGLENcNu_')
 
+_db = {
+    'flyphp': database.Connection(host='192.168.30.239',database='flyphp',user='dev_query',password='OWl32ud0zW8uGLENcNu_'),
+    'local ': database.Connection(host='localhost',database='flyphp',user='root',password='')
+}
+
+_db['flyphp']
+_db['local']
+
 def GetCustomerCount():
     sql= """select TABLE_ROWS from information_schema.TABLES
             where information_schema.TABLES.TABLE_SCHEMA = 'flyphp'
             and information_schema.TABLES.TABLE_NAME = 'fly_customer'"""
-    result = db.query(sql)
+    result = _db['flyphp'].query(sql)
     return result[0][0]
 
 def GetCusSexFromDetail():
@@ -44,9 +52,9 @@ def LocationInResult(x,list):
         #else:
             #return 0
 
-def AddAdressToResult(result,Address):
+def AddAdressToResult(result,Address,Address2):
     count = 0
-    for row in Address: #2.将address放进result
+    for row in Address: #1.将address放进result(female)
         location = LocationInResult(row[0], result)  #print "row[0]",row[0]
         if(location):
             #print row[0],'and', result[location][0]  #check consistency
@@ -54,6 +62,14 @@ def AddAdressToResult(result,Address):
             count =count+1
         else:
             result.append([row[0], 2])
+    for row in Address2: #2.将address2放进result(male)
+        location = LocationInResult(row[0], result)  #print "row[0]",row[0]
+        if(location):
+            #print row[0],'and', result[location][0]  #check consistency
+            result[location][1] = 1
+            count =count+1
+        else:
+            result.append([row[0], 1])
     print 'Overlap: ',count
 
 
@@ -73,8 +89,9 @@ for row in Detail:  #最终结果在result里，1.将detail放进result，并且
 #print "result",result
 print "Detail result——len", len(result)
 #print result
-AddAdressToResult(result,Address)  #2.将Adress放进result（女)
-print "Total result——len", len(result)
+AddAdressToResult(result,Address,Address2)  # 2.将Adress,Adress2 放进result
+print "Final result——len", len(result)
+
 
 
 for row in result:     #为什么要把qq的去掉
@@ -84,14 +101,32 @@ for row in result:     #为什么要把qq的去掉
     WHERE customer_id=%s
     """ % row[0]
     check_sf = db.query(sql)
-    #print type(check_sf[0])
-    #print type(check_sf)
-    print check_sf
-    if 'qq' in check_sf[0][0]:
-        print 'removal'
+    # print check_sf
+    # print check_sf[0] #IndexError: tuple index out of range
+    # print check_sf[0][0]
+    if check_sf and 'sf-express' in check_sf[0][0]:
+        print 'removal',check_sf
         result.remove(row)
 
 print "Total result without sf——len", len(result)
+
+customer count is 384767
+len-Detail: 5407
+len-Address: 3636
+len-Address2: 2224
+Detail result——len 5407
+Overlap:  511
+Final result——len 10756
+Total result without sf——len 10317
+
+# for row in result:     #为什么要把qq的去掉
+#     sql= """
+#     SELECT email,customer_id,realname
+#     FROM fly_customer
+#     WHERE customer_id=%s
+#     """ % row[0]
+#     check = db.query(sql)
+#     print check
 
 
 
