1662f7c5bada8196e59b0db276619c8f47fd943c
wuxuan <603491092@qq.com>
2014-07-14 18:06:00 +0800

    稍微修改了一下格式

--- SetAttriArray.py
+++ SetAttriArray.py
@@ -3,7 +3,6 @@ __author__ = 'hexiaoyu'
 import MySQLdb
 import database
 db = database.Connection(host='192.168.30.239',database='flyphp',user='dev_query',password='OWl32ud0zW8uGLENcNu_')
-
 _db = {
     'flyphp': database.Connection(host='192.168.30.239',database='flyphp',user='dev_query',password='OWl32ud0zW8uGLENcNu_'),
     'local': database.Connection(host='localhost',database='decision_making_tree',user='root',password='')
@@ -33,35 +32,26 @@ attri = []
 customer_product_table = []
 i=0
 for row in result:
-    all_product = [] #存储一个客户购买过的所有商品
+    all_product = [] #存储一个客户购买过的所有商品#其实可以删除
     all_category = [] #存储一个客户购买过的所有商品种类
     if i==0:
         order=[]  #客户的订单列表
-        sql= """
-        SELECT order_id
-        FROM fly_order
-        WHERE customer_id=%s
-        """ % row[0]
+        sql= """SELECT order_id FROM fly_order WHERE customer_id=% """ % row[0]
         FindAttri(sql,order)
         print "order",order,"/"
 
         for order_id in order:
             product = [] #当前订单下的商品列表
-            sql= """SELECT product_id
-                FROM fly_order_product
-                WHERE order_id=%d
-                """ % order_id
+            sql= """SELECT product_id  FROM fly_order_product WHERE order_id=%d """ % order_id
             FindAttri(sql,product)
             print "order_id,product:",order_id,product
             #把单个订单下的商品列表加入到总商品列表中
             all_product.extend(product)
             # print "all_product",all_product
+
             for product_id in product:
                 category = []
-                sql="""SELECT category_id
-                    FROM fly_product_to_category
-                    WHERE product_id=%d
-                    """ % product_id
+                sql="""SELECT category_id FROM fly_product_to_category WHERE product_id=%d """ % product_id
                 FindAttri(sql,category)
                 print "product_id,category:",product_id,category
                 #把单个订单下的商品列表加入到总商品列表中
